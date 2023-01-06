4b2586db768bc62beb2864a9987ee5fcee0e75b2
Bikarna Pokharel <bikarnap@gmail.com>
2023-01-07 00:40:59 +0200

    Update to add multiple props
    Multiple props can be passed to component
    
    Props can be passed as the result of JavaScript expressions
    
    Props can be passed as hard-coded strings
    
    To use results of Javascript expression,
    
    the expression must be wrapped with a pair of opening and closing curly braces

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,18 +1,21 @@
 const Hello = (props) => {
   return (
     <div>
-      <p>Hello {props.name}</p>
+      <p>Hello {props.name}, you are {props.age} years old</p>
     </div>
   )
 };
 
-const App = () => (
-  <div>
-    <h1>Greetings</h1>
-    <Hello name="GitHub"/>
-    <Hello name="Gitlab" />
-  </div>
-);
+const App = () => {
+  const age = 21;
+  return(
+    <div>
+      <h1>Greetings</h1>
+      <Hello name="Vicky" age={23 + 1} />
+      <Hello name="Harry" age={age} />
+    </div>
+  )
+};
 
 
 export default App;
