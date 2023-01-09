15422733d13823a709a32d8f5b3a47957f5f1320
Pokharel Bikarna (Ext) <ext.Bikarna.Pokharel@kalmarglobal.com>
2023-01-09 15:02:22 +0200

    Start using useState hook

--- example-state-event/src/App.js
+++ example-state-event/src/App.js
@@ -1,6 +1,12 @@
-function App(props) {
-  const { counter } = props;
+import { useState } from "react";
 
+function App(props) {
+  const [ counter, setCounter ] = useState(0);
+
+  setTimeout(
+    () => setCounter(counter + 1),
+    1000
+  );
   return (
     <div>
       {counter}
