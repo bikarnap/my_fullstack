169884c436e1e711d0e6cd1f28aef7fa3fee3c9a
Pokharel Bikarna (Ext) <ext.Bikarna.Pokharel@kalmarglobal.com>
2023-01-09 15:08:46 +0200

    Add an event handler that logs on console

--- example-state-event/src/App.js
+++ example-state-event/src/App.js
@@ -3,17 +3,16 @@ import { useState } from "react";
 function App(props) {
   const [ counter, setCounter ] = useState(0);
 
-  setTimeout(
-    () => setCounter(counter + 1),
-    1000
-  );
+  const handleClick = () => {
+    console.log('clicked');
+  };
 
-  console.log('rendering...', counter);
-  
   return (
     <div>
-      {counter}
+      <div> {counter} </div>
+      <button onClick={handleClick}>plus</button>
     </div>
+    
   );
 }
 
