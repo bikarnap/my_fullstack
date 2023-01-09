fcc51c6ab065f0ead0cfc79232ed8988e847c730
Pokharel Bikarna (Ext) <ext.Bikarna.Pokharel@kalmarglobal.com>
2023-01-09 15:12:58 +0200

    Modify event handler to change the counter
    Add a counter reset button and its event handler

--- example-state-event/src/App.js
+++ example-state-event/src/App.js
@@ -4,13 +4,18 @@ function App(props) {
   const [ counter, setCounter ] = useState(0);
 
   const handleClick = () => {
-    console.log('clicked');
+    setCounter(counter + 1);
+  };
+
+  const resetCounter = () => {
+    setCounter(0);
   };
 
   return (
     <div>
       <div> {counter} </div>
       <button onClick={handleClick}>plus</button>
+      <button onClick={resetCounter}>zero</button>
     </div>
     
   );
