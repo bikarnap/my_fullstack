b418ab0df407832483ec5fc81aae1c1cdf3e142b
Pokharel Bikarna (Ext) <ext.Bikarna.Pokharel@kalmarglobal.com>
2023-01-10 14:41:31 +0200

    Add conditional rendering

--- example-state-event/src/App.js
+++ example-state-event/src/App.js
@@ -1,5 +1,22 @@
 import { useState } from "react";
 
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    );
+  };
+
+  return (
+    <div>
+      button press history: {props.allClicks.join(' ')}
+    </div>
+  );
+};
+
+
 const App = () => {
   const [left, setLeft] = useState(0);
   const [right, setRight] = useState(0);
@@ -22,7 +39,7 @@ const App = () => {
       <button onClick={handleLeftClick}>left</button>
       <button onClick={handleRightClick}>right</button>
       {right}
-      <p>{allClicks.join(' ')}</p>
+      <History allClicks={allClicks} />
     </div>
   );
 }
