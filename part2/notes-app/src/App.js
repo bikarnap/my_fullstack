5c40368bb5cfefefdd9334a34833f87cae05a69c
Pokharel Bikarna (Ext) <ext.Bikarna.Pokharel@kalmarglobal.com>
2023-01-12 09:55:49 +0200

    Refactor to create a Note component

--- part2/notes-app/src/App.js
+++ part2/notes-app/src/App.js
@@ -1,12 +1,15 @@
+const Note = ({ note }) => <li>{note}</li>
+
 const App = ({ notes }) => {
   return (
     <div>
       <h1>Notes</h1>
       <ul>
         {notes.map(note => 
-          <li key={note.id}>
-            {note.content}
-          </li>
+          <Note 
+           key={note.id} 
+           note={note.content} 
+          />
         )}
       </ul>
     </div>
