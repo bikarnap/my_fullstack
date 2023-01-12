27e2f31376302c4035f5b9052d78765beaeb8a00
Pokharel Bikarna (Ext) <ext.Bikarna.Pokharel@kalmarglobal.com>
2023-01-12 12:58:43 +0200

    Add functionality to add a new note

--- part2/notes-app/src/App.js
+++ part2/notes-app/src/App.js
@@ -1,6 +1,27 @@
+import { useState } from 'react';
 import Note from './components/Note';
 
-const App = ({ notes }) => {
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes)
+  const [newNote, setNewNote] = useState('');
+
+  const addNote = (event) => {
+    event.preventDefault();
+    const noteObject = {
+      id: notes.length + 1,
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() < 0.5
+    };
+    setNotes(notes.concat(noteObject));
+    setNewNote('');
+
+  }
+
+  const handleOnChange = (event) => {
+    setNewNote(event.target.value);
+  }
+
   return (
     <div>
       <h1>Notes</h1>
@@ -12,6 +33,10 @@ const App = ({ notes }) => {
           />
         )}
       </ul>
+      <form onSubmit={addNote}>
+        <input onChange={handleOnChange} value={newNote}/>
+        <button type='submit'>save</button>
+      </form>
     </div>
   );
 }
