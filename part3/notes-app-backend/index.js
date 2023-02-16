f55b4ea0312e092bfe0b6572b181371b22d33fb8
Pokharel Bikarna (Ext) <ext.Bikarna.Pokharel@kalmarglobal.com>
2023-02-16 15:42:31 +0200

    Update baseurl

--- part3/notes-app-backend/index.js
+++ part3/notes-app-backend/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 const requestLogger = (req, res, next) => {
@@ -89,7 +91,7 @@ const unknownEndpoint = (req, res) => {
 
 app.use(unknownEndpoint);
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
